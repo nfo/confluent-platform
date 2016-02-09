0fdd9e4c31f2be366f4151aaae02a876c6778dca
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2016-02-09 15:31:54 -0500

    Add ability to configure Kafka's advertised.host.name. Set it to the container's IP address by default.
    Without this setting we can't connect external consumers to the
    container by specifying "localhost" as the ZK or broker list address.

--- scripts/confluent-start.sh
+++ scripts/confluent-start.sh
@@ -2,9 +2,22 @@
 
 cd /confluent-2.0.0
 
+IP=$(cat /etc/hosts | head -n1 | awk '{print $1}')
+
+kafka_cfg=./etc/kafka/server.properties
+kafka_cfg_tpl=./etc/kafka/server.properties.tpl
+
+cp $kafka_cfg $kafka_cfg_tpl
+
+cat $kafka_cfg_tpl | sed \
+	-e "s|#advertised.host.name=<hostname routable by clients>|advertised.host.name=${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
+	> $kafka_cfg
+
+rm $kafka_cfg_tpl
+
 ./bin/zookeeper-server-start ./etc/kafka/zookeeper.properties &
 sleep 5
-./bin/kafka-server-start ./etc/kafka/server.properties &
+./bin/kafka-server-start $kafka_cfg &
 sleep 5
 ./bin/schema-registry-start ./etc/schema-registry/schema-registry.properties &
 
